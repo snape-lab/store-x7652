39c450bb09d2a79e10a361fb176ad2da6be6eb7b
Manish Kumar Das <ammanishkumardas@gmail.com>
2024-07-07 17:15:26 +0530

    netlify integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,17 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import vercel from '@astrojs/vercel/serverless';
+// import vercel from '@astrojs/';
 
 import react from '@astrojs/react';
 import markdoc from '@astrojs/markdoc';
 import keystatic from '@keystatic/astro';
 
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind(), react(), markdoc(), keystatic()],
-	output: 'hybrid',
-	adapter: vercel(),
+  integrations: [tailwind(), react(), markdoc(), keystatic()],
+  output: 'hybrid',
+  adapter: netlify()
 });
