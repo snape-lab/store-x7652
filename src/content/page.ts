d6b62cb5e67f73cf2ca66a748337345cb4346ef9
Manish Kumar Das <ammanishkumardas@gmail.com>
2024-07-07 18:59:03 +0530

    fixed pages category wise

---
+++ src/content/page.ts
@@ -0,0 +1,30 @@
+export const categories = [
+	{
+		name: 'Sneakers',
+		link: '/sneakers',
+	},
+	{
+		name: 'Jerseys',
+		link: '/jerseys',
+	},
+	{
+		name: 'Football Boots',
+		link: '/football-boots',
+	},
+	{
+		name: 'Keychain',
+		link: '/keychain',
+	},
+	{
+		name: 'Football Scarfs',
+		link: '/football-scarfs',
+	},
+];
+
+export const navLinks = [
+	{
+		name: 'All',
+		link: '/all-products',
+	},
+	...categories,
+];
