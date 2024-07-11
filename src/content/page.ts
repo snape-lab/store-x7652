da20196597b21617a5134c57d45fcf2e8ee3426f
Manish Kumar Das <ammanishkumardas@gmail.com>
2024-07-12 01:10:00 +0530

    added new categories

--- src/content/page.ts
+++ src/content/page.ts
@@ -2,23 +2,45 @@ export const categories = [
 	{
 		name: 'Sneakers',
 		link: '/sneakers',
+		disabled: false,
+		singularName: 'sneaker',
 	},
 	{
 		name: 'Jerseys',
 		link: '/jerseys',
+		disabled: true,
+		singularName: 'jersey',
 	},
 	{
 		name: 'Football Boots',
 		link: '/football-boots',
+		disabled: false,
+		singularName: 'football boot',
 	},
 	{
 		name: 'Keychain',
 		link: '/keychain',
+		disabled: false,
+		singularName: 'keychain',
 	},
 	{
 		name: 'Football Scarfs',
 		link: '/football-scarfs',
+		disabled: true,
+		singularName: 'football scarf',
 	},
+	{
+		name: 'Watches',
+		link: '/watches',
+		disabled: false,
+		singularName: 'watch',
+	},
+	{
+		name: 'Running Shoes',
+		link: '/running-shoes',
+		disabled: false,
+		singularName: 'running shoe',
+	}
 ];
 
 export const navLinks = [
